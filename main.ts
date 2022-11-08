59b51002d4cc23353ac9fd4773294e02ba1bf9f8
John Gouk <johngouk@googlemail.com>
2022-11-08 12:29:38 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,15 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showString("Button A pressed")
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.No)
+    basic.pause(1000)
 })
 basic.showIcon(IconNames.Heart)
 basic.pause(2000)
 basic.forever(function () {
     basic.showString("Hello!")
-    basic.pause(1000)
     basic.showIcon(IconNames.Happy)
     basic.pause(1000)
 })
